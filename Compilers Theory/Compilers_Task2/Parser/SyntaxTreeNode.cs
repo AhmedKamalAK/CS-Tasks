6f69c30d775782d77c805f7e22ee105aeb51b09b
AhmedKamalAK <ahmed.kamal@cis.asu.edu.eg>
2015-11-11 12:58:39 +0200

    Added Compilers_Task2 project

---
+++ Compilers Theory/Compilers_Task2/Parser/SyntaxTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Scanner;
+
+namespace Parser
+{
+    class SyntaxTreeNode
+    {
+        public SyntaxTreeNode Left { get; set; }
+        public SyntaxTreeNode Right { get; set; }
+        public Token Token { get; set; }
+    }
+}
